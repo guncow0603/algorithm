e802b0fd95e929ecacae051bd5c9045908d041fb
guncow0603 <https://github.com/guncow0603>
2023-10-10 16:24:57 +0900

    배열의 평균값

--- src/Solution03.java
+++ src/Solution03.java
@@ -1,4 +1,6 @@
-class Solution03 {
+public class Solution03 {
+    //정수 n이 주어질 때, n이하의 짝수를 모두 더한 값을 return 하도록 solution 함수를 작성해주세요.
+    //짝수의 합
     public int solution(int n) {
         int answer = 0;
         for(int i=1;i<=n;i++){
