46a8c93d6c6091a87945b09bca5974b8da9aa721
guncow0603 <https://github.com/guncow0603>
2023-10-11 15:29:01 +0900

    모음제거

--- src/Solution21.java
+++ src/Solution21.java
@@ -6,14 +6,16 @@ public class Solution21 {
     public String solution(String my_string) {
         String answer = "";
         String[] replace = {"a", "e", "i", "o", "u"};
-        for (int i = 0; i < replace.length; i++) {
-            if (my_string.contains(replace[i])) {
+        for(int i = 0; i<replace.length; i++) {
+            if( my_string.contains(replace[i])) {
                 answer = my_string.replaceAll(replace[i], "");
                 my_string = answer;
             } else {
-                answer = my_string;
+                answer =my_string;
             }
         }
 
+
+        return answer;
     }
 }
