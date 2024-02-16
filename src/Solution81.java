0afd2ada588ab812dde28f0a4d71de5678d60a6e
kimgunwoo <guncow0603@gmail.com>
2024-02-16 14:53:22 +0900

    외계행성의 나이

---
+++ src/Solution81.java
@@ -0,0 +1,11 @@
+class Solution81 {
+    public String solution(int age) {
+        StringBuilder answer= new StringBuilder();
+        while(age>0){
+            answer.insert(0,(char)((age%10)+(int)'a'));
+            age/=10;
+        }
+
+        return answer.toString();
+    }
+}
