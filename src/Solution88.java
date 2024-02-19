7980e335beeb5873adeebb458cfd06237c198957
kimgunwoo <guncow0603@gmail.com>
2024-02-19 15:16:41 +0900

    배열 회전시키기

---
+++ src/Solution88.java
@@ -0,0 +1,22 @@
+class Solution88 {
+    public int[] solution(int[] numbers, String direction) {
+        int[] answer =new int[numbers.length];
+        int temp;
+        if(direction.equals("right")){
+            temp=numbers[numbers.length-1];
+            for(int i=numbers.length-1;i>0;i--){
+                numbers[i]=numbers[i-1];
+            }
+            numbers[0]=temp;
+            answer=numbers;
+        }else if(direction.equals("left")){
+            temp=numbers[0];
+            for(int i=0;i<numbers.length-1;i++){
+                numbers[i]=numbers[i+1];
+            }
+            numbers[numbers.length-1]=temp;
+            answer=numbers;
+        }
+        return answer;
+    }
+}
