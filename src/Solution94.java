112733b70bb0d5cf3c1a48a1bfeeb9dc46352e46
kimgunwoo <guncow0603@gmail.com>
2024-02-21 17:24:29 +0900

    369게임

---
+++ src/Solution94.java
@@ -0,0 +1,14 @@
+class Solution94 {
+    public int solution(int order) {
+        int answer = 0;
+        int num;
+        while(order>0){
+            num=order%10;
+            order/=10;
+            if(num%3==0&&num!=0){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
